393a886b547a79b2d4a00cea8d2facea743e250c
Igor Suvorov <hi@isuvorov.ru>
2018-01-04 10:21:23 +0200

    v3

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,5 +1,4 @@
-require('babel-core/register');
-require('babel-polyfill');
+require('@babel/register');
 const ctx = require('./config').default;
 const getWebpackConfig = require('lsk-build').getWebpackConfig;
 module.exports = getWebpackConfig(ctx);
