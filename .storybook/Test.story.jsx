6851c934558e09b8d576e4f2e46cba7a53fee45d
Igor Suvorov <hi@isuvorov.ru>
2019-02-14 23:17:36 +0200

    test

--- .storybook/Test.story.jsx
+++ .storybook/Test.story.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { ThemeContext } from '@emotion/core';
+import { ThemeProvider } from 'emotion-theming';
+import styled from '@emotion/styled';
+import Story from '../src/Story';
 
+const StyledButton = styled('button')`
+  background-color: red;
+  color: ${p => { console.log('p', p);  return p.theme.colors.primary}};
+`
 
 console.log({ThemeContext});
 
 module.exports = function ({ storiesOf, action, knob }) {
-  return storiesOf('Button', module)
+  return storiesOf('Test', module)
     .add('with text', () => (
+      <Story>
       <button onClick={action('clicked')}>Hello button</button>
+      </Story>
     ))
     .add('with some emoji', () => (
+      
       <button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></button>
+    ))
+    .add('styled', () => (
+      <Story>
+        <StyledButton>
+          123
+        </StyledButton>
+
+      </Story>
+    ))
+    .add('styled2', () => (
+      <ThemeProvider theme={{ colors: {primary: 'red'}}}>
+        <StyledButton>
+          123
+        </StyledButton>
+
+      </ThemeProvider>
     ));
 };
