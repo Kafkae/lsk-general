855b782bd85e0aa13db8b188952cd27950882782
Nikita Savyolov <ga2mer@yandex.ru>
2019-02-15 10:12:13 +0300

    update babel and storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,3 @@
-import { config } from 'storybox';
+import { configure } from 'storybox';
 import params from './params';
-config(params);
+configure(params);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,1 +1,4 @@
-module.exports = require('../tools/webpack.config')[0]
+const config = require('../tools/webpack.config')[0];
+config.module.rules[0].options = require('../babel.config.js');
+// console.log(config.module.rules);
+module.exports = config;
